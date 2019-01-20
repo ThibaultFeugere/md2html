8c2f2a6436f96a3447ddb74594ca99a3f332c4aa
Unknown <thibault.feugere@ynov.com>
2019-01-20 21:53:18 +0100

    Adding the md file to convert and the beginning of the Python script.

---
+++ converter.py
@@ -0,0 +1,8 @@
+f = open("markdown2convert.md", "r")
+
+liste = []
+
+for lettres in f:
+    liste.append(lettres)
+
+print(liste)
---
+++ markdown2convert.md
@@ -0,0 +1,5 @@
+# Title 1 (h1 in html)
+## Title 2 (h2 in html)
+### Title 3 (h3 in html)
+
+*italic*

cfef2cf28bd4073cc74d29e23f7e72ce7eba7955
flamingfurry <thibault.feugere@ynov.com>
2019-01-23 12:18:48 +0100

    Ajout de la boucle FOR qui va analyser ligne par ligne

--- converter.py
+++ converter.py
@@ -1,18 +1,31 @@
-f = open("markdown2convert.md", "r")
-h = open("index.html", "w")
+fichier_markdown = open("markdown.md", "r")
+fichier_index = open("index.html", "w")
 
-liste = []
+markdown = []
+index = []
+
+hashtag = []
 
 compteur = 0
 
-for lettres in f:
-    liste.append(lettres)
-    
-for lettres in liste:
-    h.write(lettres)
+for ligne in fichier_markdown:
+    markdown.append(ligne)
+
+for lettre in markdown[compteur]:
+    compteur += 1
+    if lettre == "#":
+        hashtag.append(lettre)
+            
+for lettres in markdown:
+    fichier_index.write("<h1>")
+    fichier_index.write(lettres)
+    fichier_index.write("</h1>")
     
 # Affichage du nombre de lignes du fichier markdown
-print("Nombre de lignes du fichier .md :", len(liste))
+print("Nombre de lignes du fichier .md :", len(markdown))
 
 # Verification de la lecture du fichier markdown
-print("La liste :\n", liste)
+print("La liste :\n", markdown)
+
+# Affichage du nombre de hashtag
+print("Valeur du titre :\n", hashtag)

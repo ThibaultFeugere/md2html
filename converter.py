1896e15d0c7a8fe6b62587f8262312da810b6b42
flamingfurry <thibault.feugere@ynov.com>
2019-01-23 10:22:11 +0100

    Ajout de la creation du fichier index.html

--- converter.py
+++ converter.py
@@ -1,12 +1,18 @@
 f = open("markdown2convert.md", "r")
+h = open("index.html", "w")
 
 liste = []
 
+compteur = 0
+
 for lettres in f:
     liste.append(lettres)
+    
+for lettres in liste:
+    h.write(lettres)
+    
+# Affichage du nombre de lignes du fichier markdown
+print("Nombre de lignes du fichier .md :", len(liste))
 
 # Verification de la lecture du fichier markdown
-print(liste)
-
-# Affichage du nombre de lignes du fichier markdown
-print(len(liste))
+print("La liste :\n", liste)
